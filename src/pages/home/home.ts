565e4537592971d7575b345247bd2213d6c213a6
Kiran Suresha <kiran.s.2528@gmail.com>
2018-03-29 20:51:32 -0400

    Initial Commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  posts: any;
+
+  constructor(public navCtrl: NavController, public http: Http) {
+
+    this.posts = null;
+
+    this.http.get('https://www.reddit.com/r/worldnews/.json')
+    .map(res => res.json())
+    .subscribe(data => {
+      this.posts = data.data.children;
+      console.log(this.posts);
+    });
+  }
+}
